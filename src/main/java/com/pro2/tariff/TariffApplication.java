b8b6e9cd6850cc5e5a5be4a2f470f884634f1a8c
user12070505 <cji010222@gmail.com>
2025-07-15 10:49:39 +0900

    관세

--- src/main/java/com/pro2/tariff/TariffApplication.java
+++ src/main/java/com/pro2/tariff/TariffApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-@EnableScheduling // 크롤러 자동 실행
+@EnableScheduling //크롤러 자동 실행
 public class TariffApplication {
     public static void main(String[] args) {
         SpringApplication.run(TariffApplication.class, args);
